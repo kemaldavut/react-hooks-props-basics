6ac4d886094a440ac10f786973295b6b1d8ecff1
Kemal DAVUT <mustafakemaldavut@gmail.com>
2022-07-31 13:47:58 +0300

    Completed assignment

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  return <div id="blog-content">
+    {props.articleText}
+    <p>{props.isPublished}</p>
+    <p>{props.minutes}</p>
+    </div>;
 }
 
 export default BlogContent;
